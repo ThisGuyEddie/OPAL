83796df221c9c4a4bd8b6ff91d51237d6e73dc8e
Eddie Rodriguez <eddie.albert.rodriguez@gmail.com>
2019-03-22 23:05:50 -0700

    Initial Commit

---
+++ OPAL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OPAL
+//
+//  Created by Eddie Rodriguez on 3/22/19.
+//  Copyright © 2019 Eddie Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
