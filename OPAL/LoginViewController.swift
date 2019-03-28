9e3ffa7b118e5196544717cd89f3f8088b75a668
Eddie Rodriguez <eddie.albert.rodriguez@gmail.com>
2019-03-28 08:59:53 -0700

    Push to master

---
+++ OPAL/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  OPAL
+//
+//  Created by Eddie Rodriguez on 3/26/19.
+//  Copyright © 2019 Eddie Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-OPALTests/Pods-OPALTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OPALTests : NSObject
+@end
+@implementation PodsDummy_Pods_OPALTests
+@end
